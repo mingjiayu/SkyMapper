a5b1e2d65a7db922c54976360a18312060db34ed
mingjiayu <91915656+mingjiayu@users.noreply.github.com>
2024-11-04 17:58:18 -0800

    Add files via upload

---
+++ StarLink/src/constants.js
@@ -0,0 +1,11 @@
+export const SAT_API_KEY = "5KQ58T-WBHNAG-22K3S2-4ZL7";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const BASE_URL = "https://symmetric-span-374017.uw.r.appspot.com"; // your backend endpoint when deploy(the same as around frontend on GAE, as we need use around backen to visit N2YO)
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
